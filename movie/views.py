4f23d50aaf615b5924b4e948105216a653d62420
hxm6621315 <hxm6621315@163.com>
2018-03-24 19:45:05 +0800

    初版，项目结构搭建。

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.http import HttpResponse
 
-from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    data = {'aaa':'bbb'}
+    print  data
+    return HttpResponse("<H1>Hello,World</H1>")
+
